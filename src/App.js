26d0842e23e8e52d9ac68460041f7a49b897d456
salimatiyeh <131699477+salimatiyeh@users.noreply.github.com>
2023-11-29 18:20:22 -0600

    form input state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,15 +26,35 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = () => {
+    console.log(tasks)
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, reminder:
+      !task.reminder } : task
+    ))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (
+        <Tasks onToggle={toggleReminder}
+        tasks={tasks} onDelete=
+        {deleteTask} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
   );
 }
